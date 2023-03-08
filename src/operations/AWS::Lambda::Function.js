78e7aba0ba6b06e204d37ff6ab246b8f7b302c16
metaory <metaory@gmail.com>
2023-03-08 14:46:28 +0800

    init

--- src/operations/AWS::Lambda::Function.js
+++ src/operations/AWS::Lambda::Function.js
@@ -1,18 +1,18 @@
-import { LambdaClient, TagResourceCommand } from "@aws-sdk/client-lambda";
+import {LambdaClient, TagResourceCommand} from '@aws-sdk/client-lambda';
 
 export default async function (physicalResourceId) {
 	const arn = `arn:aws:lambda:${global.region}:${global.accountId}:function:${physicalResourceId}`;
 
-	const client = new LambdaClient({ region });
+	const client = new LambdaClient({region});
 	const input = {
 		Resource: arn,
 		Tags: {
-			"user:aws-resource": "lambda",
-			"user:service": "MY_AWESOME_TAG_tobzzze",
+			'user:aws-resource': 'lambda',
+			'user:service': 'MY_AWESOME_TAG_tobzzze',
 		},
 	};
-	// zz
+	// Zz
 	const command = new TagResourceCommand(input);
 	const response = await client.send(command);
-	console.log("response:", response);
+	console.log('response:', response);
 }
