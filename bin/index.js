bf2c58018e8eef64495a790c61f9d52f29a030e5
metaory <metaory@gmail.com>
2023-03-14 23:05:39 +0800

    refactored

--- bin/index.js
+++ bin/index.js
@@ -30,6 +30,40 @@ console.log('modules:', Object.keys(modules));
 
 const cfn = new CFN();
 
+const processResource = async (serviceName, resource) => {
+	const {ResourceType, PhysicalResourceId} = resource;
+
+	const Key = getServiceKey(ResourceType);
+
+	logResource(Object.keys(modules), Key, ResourceType, PhysicalResourceId);
+
+	// Call the tagger module
+	if (Key in modules === false) {
+		return;
+	}
+
+	const {ServiceName} = await serviceNamePrompt(serviceName);
+	const payload = {PhysicalResourceId, ServiceName};
+
+	await modules[Key].plan(payload);
+
+	const {confirm} = await confirmInput('confirm');
+	if (confirm === false) {
+		return;
+	}
+
+	try {
+		const response = await modules[Key].default(payload);
+		console.log(chalk.green(fillTo()));
+		console.log('response:', response);
+		console.log(chalk.green(fillTo()), '\n');
+	} catch (error) {
+		console.error(chalk.red(fillTo(error.message)));
+		console.error(chalk.red(error.message));
+		console.error(chalk.red(fillTo(error.message)), '\n');
+	}
+};
+
 for (const [i, path] of templatePaths.entries()) {
 	console.log('---------------------------');
 	logWarn('template path:', path, `${i}/${templatePaths.length}`);
@@ -41,48 +75,8 @@ for (const [i, path] of templatePaths.entries()) {
 	logDanger('ServiceName:', serviceName);
 
 	const {StackResources} = await cfn.listResources(StackName);
+
 	for (const resource of StackResources) {
-		const {ResourceType, PhysicalResourceId} = resource;
-
-		const Key = getServiceKey(ResourceType);
-
-		logResource(Object.keys(modules), Key, ResourceType, PhysicalResourceId);
-
-		// Call the tagger module
-		if (Key in modules) {
-			const {ServiceName} = await serviceNamePrompt(serviceName);
-			const payload = {PhysicalResourceId, ServiceName};
-			await modules[Key].plan(payload);
-			const {confirm} = await confirmInput('confirm');
-			if (confirm === false) {
-				continue;
-			}
-
-			try {
-				const response = await modules[Key].default(payload);
-				console.error(chalk.green(fillTo()));
-				console.log('response:', response);
-				console.error(chalk.green(fillTo()));
-			} catch (error) {
-				console.error(chalk.red(fillTo(error.message)));
-				console.error(chalk.red(error.message));
-				console.error(chalk.red(fillTo(error.message)), '\n');
-			}
-		}
+		await processResource(serviceName, resource);
 	}
 }
-
-// ............................................................................
-// ············································································
-// ············································································
-// ////////////////////////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////////////////////////
-// ############################################################################
-// ############################################################################
-// ############################################################################
-// ############################################################################
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
