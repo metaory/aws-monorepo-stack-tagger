5be5dfb724a7c60a7fa94451346da22293179aa0
metaory <metaory@gmail.com>
2023-03-15 16:59:36 +0800

    added dynamodb driver

---
+++ drivers/AWS::DynamoDB.js
@@ -0,0 +1,21 @@
+import {DynamoDBClient, TagResourceCommand} from '@aws-sdk/client-dynamodb';
+
+const _input = (arn, serviceName) => ({
+	ResourceArn: arn,
+	Tags: [
+		{key: 'user:aws-resource', value: 'dynamodb'},
+		{key: 'user:service', value: serviceName},
+	],
+});
+
+export default function ({PhysicalResourceId, ServiceName}) {
+	const input = _input(PhysicalResourceId, ServiceName);
+	const client = new DynamoDBClient({region});
+	const command = new TagResourceCommand(input);
+	return client.send(command);
+}
+
+export function plan({PhysicalResourceId, ServiceName}) {
+	console.log(_input(PhysicalResourceId, ServiceName));
+}
+
